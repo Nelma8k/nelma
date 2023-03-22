dda2d426b62844e3965168a273827be9fbad475a
Nelma8k <nbinguane@gmail.com>
2023-03-22 10:12:26 -0300

    Initial commit

---
+++ home.php
@@ -0,0 +1,18 @@
+<?php
+include_once("header.php");
+?>
+
+
+<div class="content-wrapper">
+          <div class="row purchace-popup">
+           
+          </div>
+          
+          
+          <h2>hk</h2>
+          
+          
+ </div>
+ <?php
+include_once("footer.php");
+?>
